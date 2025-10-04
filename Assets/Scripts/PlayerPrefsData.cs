cff1307be163a8ae7b299ff39fae73192a6861a0
Jado <jade.bouiges@edu.esiee-it.fr>
2025-10-04 11:55:33 +0200

    feat: add main menu

--- Assets/Scripts/PlayerPrefsData.cs
+++ Assets/Scripts/PlayerPrefsData.cs
@@ -5,6 +5,7 @@ public class PlayerPrefsData
     // Succes
 
     private const string HAS_QUIT_THE_GAME = "HAS_QUIT_THE_GAME";
+    private const string HAS_RESPECTED_CREATORS = "HAS_RESPECTED_CREATORS";
 
     // Other
 
