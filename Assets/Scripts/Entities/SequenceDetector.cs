698bbe24f749571d58330cdd2e07ce747ab46ec1
Jado <jade.bouiges@edu.esiee-it.fr>
2025-10-05 10:58:39 +0200

    fix: ready for build

--- Assets/Scripts/Entities/SequenceDetector.cs
+++ Assets/Scripts/Entities/SequenceDetector.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.Requests;
-using UnityEditor.SearchService;
 using UnityEngine;
 
 public class SequenceDetector : MonoBehaviour
