e6dde676e7dfaa30867931696b08339e56d5e616
Jado <jade.bouiges@edu.esiee-it.fr>
2025-10-06 21:03:15 +0200

    add shakycam on chest

--- Assets/Scripts/FixedObjects/Chest.cs
+++ Assets/Scripts/FixedObjects/Chest.cs
@@ -4,8 +4,10 @@ public class Chest : MonoBehaviour, IInteractable
 {
     [SerializeField] SuccessMapManager _success;
 
+    [SerializeField] ShakyCam _shakyCam;
 	public void Interact()
     {
+        _shakyCam.ShakyCameCustom(0.15f, 0.3f);
         _success.LaunchSuccessAnim(PlayerPrefsData.CHEST_SUCCESS);
     }
 }
