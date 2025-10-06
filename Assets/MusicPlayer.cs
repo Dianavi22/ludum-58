2a8667ab9c47fa7017cb65e229ab9eac1588ed86
Martin Berthier <martin.berthier.mb@gmail.com>
2025-10-06 23:25:09 +0200

    better music

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -1,7 +1,21 @@
+using System;
+using UnityEngine;
+
 public class MusicPlayer : BasicAudioListener
 {
+    [SerializeField] private float _timeCode = 26;
+
     protected override void UpdateVolume(float value)
     {
         _source.volume = value * 0.6f;
+    }
+
+    private void Update()
+    {
+        if (!_source.isPlaying)
+        {
+            _source.time = _timeCode;
+            _source.Play();
+        }
     }
 }
